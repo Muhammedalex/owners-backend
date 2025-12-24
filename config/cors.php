60c0d625ba139f0a833b8f22226ce81290f321f2
Muhammedalex <146328429+Muhammedalex@users.noreply.github.com>
2025-12-24 23:13:25 +0200

    pdf-excel

--- config/cors.php
+++ config/cors.php
@@ -7,6 +7,12 @@ return [
 
     'allowed_origins' => [
         'http://localhost:3000',
+        'http://localhost:5173',
+        'https://owner.iv-erp.com',
+        'https://aljanoubia.com',
+        'https://amazingwill.sa',
+        'http://127.0.0.1:3000',
+        'http://127.0.0.1:5173',
     ],
 
     'allowed_headers' => ['*'],
